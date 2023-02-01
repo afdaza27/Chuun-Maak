2b16f42e8ade62be98e3e93a877137fa041fdb14
LuisaRC <laidiotkilljoy@gmail.com>
2023-01-31 20:04:14 -0500

    Functional Pong Base

---
+++ GGJ2023/Assets/Pong/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float speed = 7f;
+    [SerializeField] private bool isPaddle1;
+    private float yBound = 3.75f;
+    void Update()
+    {
+        float movement;
+        if (isPaddle1)
+        {
+            movement = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            movement = Input.GetAxisRaw("Vertical2");
+        }
+        //transform.position += new Vector3(0, movement * speed * Time.deltaTime, 0);
+        Vector2 paddlePosition = transform.position;
+        paddlePosition.y = Mathf.Clamp(paddlePosition.y + movement * speed * Time.deltaTime, -yBound, yBound);
+        transform.position = paddlePosition;
+    }
+}
