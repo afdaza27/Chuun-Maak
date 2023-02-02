681f70c12a028e0e16264af7688cc4ebe5bf68c0
afdaza27 <af.daza@uniandes.edu.co>
2023-02-01 23:19:33 -0500

    Pong trail and mechanics functional

--- GGJ2023/Assets/Pong/Scripts/Ball.cs
+++ GGJ2023/Assets/Pong/Scripts/Ball.cs
@@ -7,17 +7,21 @@ public class Ball : MonoBehaviour
 {
     [SerializeField] private float initialVelocity = 4f;
     [SerializeField] private float velocityMultiplier = 1.1f;
+    [SerializeField] private ParticleSystem particleSystem;
     private Rigidbody2D ballRb;
     private bool ableToScore;
+    private bool enemyAbleToScore;
     void Start()
     {
         ballRb = GetComponent<Rigidbody2D>();
         ableToScore = false;
+        enemyAbleToScore = false;
         Launch();
     }
 
     private void Launch()
     {
+        particleSystem.Stop();
         float xVelocity = Random.Range(0, 2) == 0 ? 1 : -1;
         float yVelocity = Random.Range(0, 2) == 0 ? 1 : -1;
         ballRb.velocity = new Vector2(xVelocity, yVelocity) * initialVelocity;
@@ -32,13 +36,27 @@ public class Ball : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Hoop"))
         {
-            ableToScore = true;
+            ParticleSystem.MainModule main = particleSystem.main;
+            if (ballRb.velocity.x < 0)
+            {
+                ableToScore = true;
+                enemyAbleToScore = false;
+                main.startColor = new ParticleSystem.MinMaxGradient(new Color(0.01619794f, 0.2641509f, 0.06578854f), new Color(0f,1f, 0.06428576f));
+            }
+            else
+            {
+                enemyAbleToScore = true;
+                ableToScore = false;
+                main.startColor = new ParticleSystem.MinMaxGradient(new Color(0.2627451f, 0.02471273f, 0.01568628f), new Color(0.9811321f, 0.008964817f, 0f));
+            }
+            particleSystem.Play();
         }
-        if(collision.gameObject.CompareTag("Goal1") && ableToScore)
+        else if(collision.gameObject.CompareTag("Goal1") && enemyAbleToScore)
         {
             PongGameManager.Instance.Paddle2Scored();
             PongGameManager.Instance.Restart();
             ableToScore = false;
+            enemyAbleToScore = false;
             Launch();
         }
         else if(collision.gameObject.CompareTag("Goal2") && ableToScore)
@@ -46,24 +64,17 @@ public class Ball : MonoBehaviour
             PongGameManager.Instance.Paddle1Scored();
             PongGameManager.Instance.Restart();
             ableToScore = false;
-            Launch();
-        }
-        else if (collision.gameObject.CompareTag("Goal1"))
-        {
-            Launch();
-        }
-        else if (collision.gameObject.CompareTag("Goal2"))
-        {
+            enemyAbleToScore = false;
             Launch();
         }
         else
         {
+            PongGameManager.Instance.Restart();
             Launch();
         }
     }
 
     void Update()
     {
-        
     }
 }
